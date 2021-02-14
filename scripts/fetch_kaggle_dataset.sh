3257d315637570e5570af5930728104164fa806b
Shay Ben-Sasson <shaybensasson@gmail.com>
2021-02-14 16:42:08 +0200

    fixed fetch kaggle old pwd

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+OLDPWD=$(pwd)
 DB=house-prices-advanced-regression-techniques
 DEST=packages/regression_model/regression_model/datasets
 (cd $DEST || exit 1) && kaggle competitions download -c $DB -p . && unzip $DB.zip
-cd - || exit 1
+cd $OLDPWD || exit 1
