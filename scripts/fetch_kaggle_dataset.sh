36941a3a0cafc8e1148ecd5381d017e8a98726b6
Shay Ben-Sasson <shaybensasson@gmail.com>
2021-02-14 16:47:34 +0200

    expkicit path

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,6 +2,6 @@
 
 OLDPWD=$(pwd)
 DB=house-prices-advanced-regression-techniques
-DEST=packages/regression_model/regression_model/datasets
+DEST=/home/circleci/project/packages/regression_model/regression_model/datasets
 (cd $DEST || exit 1) && kaggle competitions download -c $DB -p . && unzip $DB.zip
 cd $OLDPWD || exit 1
