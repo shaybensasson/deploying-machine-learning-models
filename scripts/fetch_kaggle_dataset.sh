aa9d2308c814b7bfa5ffcde6bef06d1d06d87a6d
Shay Ben-Sasson <shaybensasson@gmail.com>
2021-02-14 16:36:51 +0200

    fetch added cd

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,4 +2,5 @@
 
 DB=house-prices-advanced-regression-techniques
 DEST=packages/regression_model/regression_model/datasets
-cd $DEST && kaggle competitions download -c $DB -p . && unzip $DB.zip
+(cd $DEST || exit 1) && kaggle competitions download -c $DB -p . && unzip $DB.zip
+cd - || exit 1
